9953efa9221427dac89d893b84e0a09d16e35d27
Jason Braswell <jason.braswell@knights.ucf.edu>
2015-11-08 15:31:35 -0500

    Initial sidebar menu setup

---
+++ ghoul/OurViewController.swift
@@ -0,0 +1,39 @@
+//
+//  OurViewController.swift
+//  ghoul
+//
+//  Created by Student on 11/8/15.
+//  Copyright (c) 2015 deadsquad. All rights reserved.
+//
+
+import UIKit
+
+class OurViewController: UIViewController {
+
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
