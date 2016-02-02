4b8ad278f01e09c722b8ee02eedbfd0cd8bee775
Student <student@Students-iMac.local>
2016-02-02 16:21:31 -0500

    Beginnings of communication protocol

---
+++ ghoul/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  ghoul
+//
+//  Created by Student on 2/2/16.
+//  Copyright (c) 2016 deadsquad. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let SOH = "\u{0001}"
+    static let STX = "\u{0002}"
+    static let ETX = "\u{0003}"
+    static let EOT = "\u{0004}"
+    static let kMessageNumber = "MSGNO"
+    static let kErrorPrefix = "ERR_"
+    static let kBadMessage = "ERR_BAD_MSG"
+    static let kResendMessage = "ERR_RESEND_MSG"
+    static let kSetFrequency = "SET_FREQ"
+    static let kOkay = "OKAY"
+    static let kFrequencyErrorPrefix = "ERR_FREQ"
+    static let kRunTrace = "RUN_TRACE"
+    static let kTraceResponseHeader = "TRACE"
+    static let kTraceResponseTail = "DONE"
+}
--- ghoul/NetworkGPRDevice.swift
+++ ghoul/NetworkGPRDevice.swift
@@ -10,7 +10,9 @@ import Foundation
 
 class NetworkGPRDevice : GPRDataSource {
     //TODO: properties and initializer(s) for actual WiFi connection
-    private var stream: NSInputStream
+    private var inputStream: NSInputStream
+    private var outputStream: NSOutputStream
+    var messageNumber: Int
     
     internal var dataStream: NSInputStream {
         get {
@@ -22,11 +24,40 @@ class NetworkGPRDevice : GPRDataSource {
     // MARK: initialization
     init?() {
         // TODO should actually be passed connection (socket?) to device and init based on that
-        stream = NSInputStream()
+        inputStream = NSInputStream()
+        outputStream = NSOutputStream()
+        
+        messageNumber = 1;
     }
     
     
     // MARK: functions
+    private func send(msg: String) -> Bool {
+        if(msg.isEmpty) {
+            return true
+        }
+        
+        if(!isConnected()) {
+            return false
+        }
+        
+        let encodedArray = [UInt8](msg.utf8)
+        return outputStream.write(encodedArray, maxLength: encodedArray.count) > 0
+        
+        
+        //outputStream.write(UnsafePointer<UInt8>(msg), maxLength: msg.lengthOfBytesUsingEncoding(<#encoding: NSStringEncoding#>))
+    }
+    
+    func sendMessage(msg: String) -> Bool {
+        if(msg.isEmpty) {
+            return false
+        }
+        
+        let formattedMessage = (messageNumber++).description
+            + " " + Constants.STX + msg + Constants.EOT
+        
+        return send((messageNumber++).description + " " + msg)
+    }
     
     func isConnected() -> Bool {
         //TODO: stub
@@ -53,6 +84,7 @@ class NetworkGPRDevice : GPRDataSource {
     func setFrequency(hertz: UInt) -> Bool {
         // should require source to stop reading before changing frequency
         // perhaps should be async and not return bool yet
+        
         
         return false
     }
