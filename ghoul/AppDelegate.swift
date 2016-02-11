2946ed6c0e6371333e26f12fe498029948761dff
“lrosa007” <lrosa008@gmail.com>
2016-02-10 23:12:13 -0500

    got the menu button in there

--- ghoul/AppDelegate.swift
+++ ghoul/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        UINavigationBar.appearance().barTintColor = UIColor.blueColor()
+        //UINavigationBar.appearance().barTintColor = UIColor.blueColor()
         
         return true
     }
